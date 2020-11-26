a34cfb19b4922cf7b844ca04cc32e71ffae54ab6
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2020-11-26 16:22:14 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-import re
-import requests
-from bs4 import BeautifulSoup
+from models.item import Item
 
-URL = "https://www.johnlewis.com/2020-apple-ipad-pro-11-inch-a12z-bionic-ios-wi-fi-128gb/space-grey/p4949052"
-TAG_NAME = "p"
-QUERY = {"class": "price price--large"}
 
-response = requests.get(URL)
-content = response.content
-soup = BeautifulSoup(content, "html.parser")
-element = soup.find(TAG_NAME, QUERY)
-string_price = element.text.strip()
+url = "https://www.johnlewis.com/2020-apple-ipad-pro-11-inch-a12z-bionic-ios-wi-fi-128gb/space-grey/p4949052"
+tag_name = "p"
+query = {"class": "price price--large"}
 
-pattern = re.compile(r"(\d*?,?\d+\.\d\d)")
-match = pattern.search(string_price)
-found_price = match.group(1)
-without_commas = found_price.replace(",", "")
-price = float(without_commas)
-
-print(match.group(1))
+item = Item(url, tag_name, query)
+item2 = Item(url, tag_name, query)
+print(item.load_price())
---
+++ item.py
@@ -0,0 +1,26 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+class Item:
+    def __init__(self, url, tag_name, query):
+        self.url = url
+        self.tag_name = tag_name
+        self.query = query
+        self.price = None
+
+    def load_price(self):
+        response = requests.get(self.url)
+        content = response.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find(self.tag_name, self.query)
+        string_price = element.text.strip()
+
+        pattern = re.compile(r"(\d*?,?\d+\.\d\d)")
+        match = pattern.search(string_price)
+        found_price = match.group(1)
+        without_commas = found_price.replace(",", "")
+        self.price = float(without_commas)
+        return self.price
+
