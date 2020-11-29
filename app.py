15d92541b6ec45aaecf0a94d4ee92c8bf1f62712
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2020-11-28 20:44:27 -0700

    Add files via upload
    added new item.html. added to app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from models.alert import Alert
+import json
+from flask import Flask, render_template, request
+from models.item import Item
+
+app = Flask(__name__)
 
 
-alert = Alert("bb77bb9ff42c4a64bab4ce47a523de21", 2000)
-alert.save_to_mongo()
+@app.route('/', methods=['GET', 'POST'])
+def new_item():
+    if request.method == 'POST':
+        url = request.form['url']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
 
+        Item(url, tag_name, query).save_to_mongo()
+
+    return render_template('new_item.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
